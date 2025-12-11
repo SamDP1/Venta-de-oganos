14c04ea2de2569d32e42a9bd6af1394b8ff7f118
sanreo00 <sanreo0.0y@gmail.com>
2025-12-11 13:41:04 -0500

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <vector>
+#include <string>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -8,18 +11,43 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 }
 
+struct itemPrice
+{
+    std::string itemName;
+    int itemPrice;
+};
+
+//
+
+std::vector<itemPrice> v;
+int total = 0;
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+// botones de navegacion
+
 void MainWindow::on_boton_vercarrito_clicked()
 {
     ui->stackedWidget->setCurrentIndex(3);
+
+
+    std::string str;
+
+    for (int i = 0;i< v.size();i++)
+    {
+        str= v[i].itemName +"\t\t" + std::to_string(v[i].itemPrice);
+        ui->itemsinCartList->addItem(str.c_str());
+        total +=v[i].itemPrice;
+    }
+    str= std::to_string(total);
+    ui->totalcarrito->setText(str.c_str());
 }
 
 
 void MainWindow::on_boton_volver_clicked()
+
 {
     ui->stackedWidget->setCurrentIndex(0);
 }
@@ -40,5 +68,27 @@ void MainWindow::on_boton_confirmar_clicked()
 void MainWindow::on_boton_next_clicked()
 {
     ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+// agrgar al carrito
+
+void MainWindow::on_add_orejas_clicked()
+{
+    itemPrice ip;
+    ip.itemName = "orejas";
+    ip.itemPrice = 30000;
+
+    v.push_back(ip);
+}
+
+
+void MainWindow::on_add_corazon_clicked()
+{
+    itemPrice ip;
+    ip.itemName = "corazon";
+    ip.itemPrice = 85000;
+
+    v.push_back(ip);
 }
 
